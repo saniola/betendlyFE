ccb78cc017b8f40d1fce74f04c48e69b3939718d
Pasha Matchak <pavlo@chess.com>
2025-11-09 13:07:34 +0200

    layout + header + normilize css

---
+++ src/components/header.vue
@@ -0,0 +1,85 @@
+<template>
+  <div :class="$style.component">
+    <RouterLink to="/">
+      <img
+        alt="Loading..."
+        height="30"
+        src="@/assets/logo.png"
+        width="115"
+        :class="$style.logo" />
+    </RouterLink>
+
+    <div>
+      <template v-if="!isLoggedIn">
+        <v-btn
+          v-if="!['signup'].includes(route.name as string)"
+          color="white"
+          variant="outlined"
+          class="text-none ms-2"
+          :to="{ name: 'signup' }"
+          @click="onSignup">
+          Sign Up
+        </v-btn>
+
+        <v-btn
+          v-if="!['login'].includes(route.name as string)"
+          color="white"
+          variant="outlined"
+          class="text-none ms-2"
+          :to="{ name: 'login' }"
+          @click="onLogin">
+          Login
+        </v-btn>
+      </template>
+
+      <template v-else>
+        <v-avatar size="36" class="ms-2">
+          <v-img :src="user.avatar" alt="User avatar" />
+        </v-avatar>
+
+        <span class="ms-2 text-white font-weight-medium">{{ user.name }}</span>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const isLoggedIn = ref(false);
+
+const user = ref({
+  name: 'Jane Doe',
+  avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
+});
+
+const onSignup = () => {
+  console.log('Navigate to signup');
+};
+
+const onLogin = () => {
+  console.log('Navigate to login');
+};
+</script>
+
+<style module lang="scss">
+.component {
+  align-items: center;
+  background-color: #1976d2;
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+}
+
+.logo {
+  width: auto;
+}
+</style>
---
+++ src/controllers/layout.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="$style.component">
+    <Header />
+
+    <slot />
+
+    <div
+      v-if="mainState.isLoading"
+      :class="$style.loader">
+      <img
+        alt="Loading..."
+        height="50"
+        src="@/assets/loader.gif"
+        width="50" />
+
+        <p
+          :class="$style.text"
+          v-text="'Please wait. Loading...'" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from '@/components/header.vue';
+import { mainState } from '@/state';
+</script>
+
+<style module lang="scss">
+.component {
+  background-color: #fff;
+  height: 100%;
+  padding-top: 52px;
+}
+
+.loader {
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.95);
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+}
+
+.text {
+  color: #000;
+  padding-top: 10px;
+}
+</style>
