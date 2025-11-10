691ee4d6eee0f91b32a70faea8c879c111b72845
Pasha Matchak <pavlo@chess.com>
2025-11-10 14:33:11 +0200

    Filters

--- src/controllers/home.vue
+++ src/controllers/home.vue
@@ -1,4 +1,12 @@
 <template>
+  <Filters
+    :cities
+    :tags
+    @city-change="onCityChange"
+    @tag-change="onTagChange"
+    @alphabet-change="onAlphabetChange"
+    @rating-change="onRatingChange" />
+
   <ul :class="$style.component">
     <MasterCard
       v-for="(master, index) in mainState.masters"
@@ -18,15 +26,53 @@
 <script setup lang="ts">
 import { fetchMasters } from '@/actions/fetch-masters';
 import { setCurrentPage } from '@/actions/set-current-page';
+import Filters from '@/components/filters.vue';
 import MasterCard from '@/components/master-card.vue';
 import { mainState } from '@/state';
+import type { Master } from '@/types/master';
+import { computed } from 'vue';
 
 fetchMasters();
+
+const cities = computed(() => {
+  const arr: string[] = [];
+
+  mainState.masters.forEach((item: Master) => {
+    if (item.address && !arr.includes(item.address)) {
+      arr.push(item.address);
+    }
+  });
+
+  return arr;
+});
+
+const tags = computed(() => {
+  const arr: string[] = [];
+
+  mainState.masters.forEach((item: Master) => {
+    item.skills.forEach((skill: string) => {
+      if (!arr.includes(skill)) {
+        arr.push(skill);
+      }
+    });
+  });
+
+  return arr;
+});
 
 function onPageChange(page: number) {
   setCurrentPage(page);
   fetchMasters();
 }
+
+function onCityChange(city: string) {}
+
+function onTagChange(tag: string) {}
+
+function onAlphabetChange(alphabet: string) {}
+
+function onRatingChange(rating: string) {}
+
 </script>
 
 <style module lang="scss">
