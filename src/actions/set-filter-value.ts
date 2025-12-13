d2b156ca2a1ad477289f502dcf5e7e9f03a7f735
Pasha Matchak <pavlo@chess.com>
2025-12-13 09:53:32 +0200

    Reset pagination on filter change

--- src/actions/set-filter-value.ts
+++ src/actions/set-filter-value.ts
@@ -1,6 +1,8 @@
 import { mainState } from '@/state';
 import type { Filter } from '@/types/filter';
+import { setCurrentPage } from '@/actions/set-current-page';
 
 export function setFilterValue(key: keyof Filter, value: string) {
   mainState.filter[key] = value;
+  setCurrentPage(1);
 }
